c5ca981678bebbb8b98af7e733ca75d2528fb031
aigamegodot <aigamegodot@gmail.com>
2025-05-20 17:43:53 +0400

    main.py

--- main.py
+++ main.py
@@ -38,3 +38,7 @@ def webhook():
 @app.route('/')
 def home():
     return 'Bot is running!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
